2918e1e349d0f22527cc3347723d58cede2b9d10
thomaswang0822 <thomaswang0822@outlook.com>
2024-04-21 16:40:48 -0700

    1024 to 512, otherwise too slow; naive ref;

--- step4/test/run_bl_dgemm.sh
+++ step4/test/run_bl_dgemm.sh
@@ -8,7 +8,7 @@ export KMP_AFFINITY=compact  #Rule to bind core to thread for OMP thread with In
 export OMP_NUM_THREADS=1     #Set OMP number of threads for parallel version
 export BLISLAB_IC_NT=1       #Set BLISLAB number of threads for parallel version
 k_start=16
-k_end=1024
+k_end=512
 k_blocksize=16
 echo "result=["
 echo -e "%m\t%n\t%k\t%MY_GFLOPS\t%REF_GFLOPS"
